23c231500509d39bfd84cf60e5ca2535ad0d2808
Bree Swn <suwanan.cs@hotmail.com>
2025-01-30 14:26:03 +0700

    finish scroll event

--- src/App.js
+++ src/App.js
@@ -4,19 +4,47 @@ import "./App.css";
 
 function App() {
   const apiKey = `8UQXGA5eCF04KJ0YgIVks5yPYrYEnP7v8xEcWIpSoRI`;
-  const apiUrl = `https://api.unsplash.com/photos/?client_id=${apiKey}&page=1`;
-
   const [photos, setPhotos] = useState([]);
+  const [page, setPage] = useState(1); //เมื่อมีการเปลี่ยนแปลงค่า page ให้เรียกใชังาน fetchImage
+  const [isLoading, setIsLoading] = useState(false);
 
   //* เมื่อมีการเรียกใช้ fetchImage หรือการเรียกใช้งานรูปภาพจาก unsplash รอ server ส่งข้อมูลกลับมา และเก็บ state ไว้ใน photos
   const fetchImage = async () => {
-    const res = await fetch(apiUrl);
-    const data = await res.json(); //ตัวแปร Array ก้อนนึง
-    setPhotos(data);
+    setIsLoading(true);
+    try {
+      const apiUrl = `https://api.unsplash.com/photos/?client_id=${apiKey}&page=${page}`;
+      const res = await fetch(apiUrl);
+      const data = await res.json(); //ตัวแปร Array ก้อนนึง
+      setPhotos((oldData) => {
+        return [...oldData, ...data];
+      });
+    } catch (err) {
+      console.log(err);
+    }
+    setIsLoading(false);
   };
 
+  //* ดึงข้อมูลรูปภาพหลังจากที่ state page มีการเปลี่ยนค่า
   useEffect(() => {
     fetchImage();
+    // eslint-disable-next-line
+  }, [page]); 
+
+  //* จัดการ scroll หน้าจอแอพ
+  useEffect(() => {
+    const event = window.addEventListener("scroll", () => {
+      if (
+        window.innerHeight + window.scrollY >
+        document.body.offsetHeight - 20 && !isLoading
+      ) {
+        //เพิ่มค่าให้กับ page state
+        setPage((oldPage) => {
+          return oldPage + 1;
+        });
+      }
+    });
+    return () => window.removeEventListener("scroll", event);
+    // eslint-disable-next-line
   }, []);
 
   return (

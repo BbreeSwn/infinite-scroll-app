4f79075e4ee0dcaa1f16d66ffbb19a0c2102f9d4
Bree Swn <suwanan.cs@hotmail.com>
2025-01-30 12:34:06 +0700

    create-api-key

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import PhotoComponent from "./components/PhotoComponent";
 
 function App() {
+  const apiKey = `8UQXGA5eCF04KJ0YgIVks5yPYrYEnP7v8xEcWIpSoRI`
+  const apiUrl = `https://api.unsplash.com/photos/?client_id=${apiKey}`
   return (
    <main>
     <h1>Infinite Scroll photo | Unsplash API</h1>

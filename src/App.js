25e26e93ee5ad2c080f2c4433841d8f2d79d2634
Bree Swn <suwanan.cs@hotmail.com>
2025-01-30 13:05:42 +0700

    show-photocomponent

--- src/App.js
+++ src/App.js
@@ -1,27 +1,34 @@
 import PhotoComponent from "./components/PhotoComponent";
-import {useEffect, useState} from 'react'
+import { useEffect, useState } from "react";
 
 function App() {
-  const apiKey = `8UQXGA5eCF04KJ0YgIVks5yPYrYEnP7v8xEcWIpSoRI`
-  const apiUrl = `https://api.unsplash.com/photos/?client_id=${apiKey}&page=1`
+  const apiKey = `8UQXGA5eCF04KJ0YgIVks5yPYrYEnP7v8xEcWIpSoRI`;
+  const apiUrl = `https://api.unsplash.com/photos/?client_id=${apiKey}&page=1`;
 
-  const [photos , setPhotos] = useState([])
+  const [photos, setPhotos] = useState([]);
 
-  //* เมื่อมีการเรียกใช้ fetchImage ให้รอข้อมูลรูปภาพก่อน
-  const fetchImage = async()=>{
-    const res = await fetch(apiUrl) 
-    const data = await res.json() //ตัวแปร Array ก้อนนึง
-    setPhotos(data)
-  }
-useEffect(()=>{
-  fetchImage()
-},[])
+  //* เมื่อมีการเรียกใช้ fetchImage หรือการเรียกใช้งานรูปภาพจาก unsplash รอ server ส่งข้อมูลกลับมา และเก็บ state ไว้ใน photos
+  const fetchImage = async () => {
+    const res = await fetch(apiUrl);
+    const data = await res.json(); //ตัวแปร Array ก้อนนึง
+    setPhotos(data);
+  };
+
+  useEffect(() => {
+    fetchImage();
+  }, []);
 
   return (
-   <main>
-    <h1>Infinite Scroll photo | Unsplash API</h1>
-    <PhotoComponent />
-   </main>
+    <main>
+      <h1>Infinite Scroll photo | Unsplash API</h1>
+      <section className="photos">
+<div className="display-photo">
+  {photos.map((data,index)=>{
+    return <PhotoComponent key={index} {...data} />
+  })}
+</div>
+      </section>
+    </main>
   );
 }
 

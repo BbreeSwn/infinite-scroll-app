c1fea672b7a4a38dbab1145ef9397a3a086f8b46
Bree Swn <suwanan.cs@hotmail.com>
2025-01-30 12:54:49 +0700

    fetch-image-data

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
 import PhotoComponent from "./components/PhotoComponent";
+import {useEffect, useState} from 'react'
 
 function App() {
   const apiKey = `8UQXGA5eCF04KJ0YgIVks5yPYrYEnP7v8xEcWIpSoRI`
-  const apiUrl = `https://api.unsplash.com/photos/?client_id=${apiKey}`
+  const apiUrl = `https://api.unsplash.com/photos/?client_id=${apiKey}&page=1`
+
+  const [photos , setPhotos] = useState([])
+
+  //* เมื่อมีการเรียกใช้ fetchImage ให้รอข้อมูลรูปภาพก่อน
+  const fetchImage = async()=>{
+    const res = await fetch(apiUrl) 
+    const data = await res.json() //ตัวแปร Array ก้อนนึง
+    setPhotos(data)
+  }
+useEffect(()=>{
+  fetchImage()
+},[])
+
   return (
    <main>
     <h1>Infinite Scroll photo | Unsplash API</h1>
